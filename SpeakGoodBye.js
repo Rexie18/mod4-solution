aaa94e91ec947d9e18e1d3683b1efff04566d0d0
Rexie18 <ro@students.kiron.ngo>
2017-02-22 22:21:06 +0100

    Update SpeakGoodBye.js

--- SpeakGoodBye.js
+++ SpeakGoodBye.js
@@ -1,6 +1,6 @@
 (function (window) {
   var byeSpeaker = {};
-  byeSpeaker.speak = name;
+  byeSpeaker.speak = names;
   var speakWord = "Good Bye";
     byeSpeaker.speak = function () {
     console.log(speakWord + byeSpeaker.speak);
