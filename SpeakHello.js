6d2b12a653f100164470f662e96fc1f7e45a791d
Rexie18 <ro@students.kiron.ngo>
2017-02-23 19:27:49 +0100

    Update SpeakHello.js

--- SpeakHello.js
+++ SpeakHello.js
@@ -26,7 +26,7 @@ See Lecture 52, part 2 */
 helloSpeaker.speak = function() {
   console.log(speakWord  + helloSpeaker.name);
   }
-  window.helloSpeaker.speak = helloSpeaker;
+  helloSpeaker.speak = helloSpeaker;
 })(window);
 
 /* STEP 5: Expose the 'helloSpeaker' object 
