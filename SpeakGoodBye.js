10a8e8aeca118dcafc143756095e3436c988a2d6
Rexie18 <ro@students.kiron.ngo>
2017-02-24 18:25:42 +0100

    Update SpeakGoodBye.js

--- SpeakGoodBye.js
+++ SpeakGoodBye.js
@@ -22,7 +22,7 @@
  See Lecture 52, part 2
 */
 byeSpeaker.speak = function (name) {
-  console.log(speakWord + byeSpeaker.names);
+  console.log(speakWord + " " + byeSpeaker.names);
   }
 window.byeSpeaker = byeSpeaker;
 })(window);
